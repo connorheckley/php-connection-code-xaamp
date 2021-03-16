1c8e156df514c53bff81af3da2cb164f2a108e16
Connor Heckley <58855366+connorheckley@users.noreply.github.com>
2021-03-16 21:06:37 +0000

    Add files via upload

---
+++ firstscript.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!";
+?>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+
+</body>
+</html>
